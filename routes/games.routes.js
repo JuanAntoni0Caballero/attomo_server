147473eb80369d8558ee934104ff9239a8f0889a
Juan Antonio Caballero <j.antoniocaballero@hotmail.es>
2023-03-21 17:06:53 +0100

    verify

--- routes/games.routes.js
+++ routes/games.routes.js
@@ -5,19 +5,16 @@ const Game = require("../models/Game.model");
 router.get('/getAllGames', (req, res, next) => {
 
   // const { name, category, image, stars } = req.query
-
   Game
     .find()
     .sort()
     .then(response => res.json(response))
     .catch(err => next(err))
-
 })
 
 router.get('/getOneGame/:game_id', (req, res, next) => {
 
   const { game_id } = req.params
-
   Game
     .findById(game_id)
     .then(response => res.json(response))
@@ -57,7 +54,6 @@ router.delete('/deleteGame/:game_id', (req, res, next) => {
     .then(response => res.json(response))
     .catch(err => next(err))
 })
-87
 
 router.put('/likeGame/:game_id/:user_id', (req, res, next) => {
 
