9adfc62dc5869ae5f9be9793c3438bea67db7011
Juan Antonio Caballero <j.antoniocaballero@hotmail.es>
2023-06-27 18:50:16 +0200

    auth

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,49 @@
 const router = require("express").Router();
+const User = require("../models/User.model");
+
 
 router.get("/", (req, res, next) => {
     res.json("Ruta de usuario");
 });
 
-module.exports = router;
+
+router.post('/signup', (req, res, next) => {
+    User
+        .create({ username, email, password })
+        .then(() => res.sendStatus(201))
+        .catch(err => next(err));
+
+})
+
+
+router.post('/login', (req, res, next) => {
+
+    const { email, password } = req.body;
+
+
+    if (email === '' || password === '') {
+        res.status(400).json({ errorMessages: ["Email y contraseña requeridos."] })
+        return
+    }
+
+    User
+        .findOne({ email })
+        .then(user => {
+            if (!user) {
+                res.status(401).json({ errorMessages: ["Usuario no registrado."] })
+                return
+            }
+
+            if (user.validatePassword(password)) {
+                res.status(200).json
+            } else {
+                res.status(401).json({ errorMessages: ['Contraseña incorrecta.'] })
+            }
+        })
+        .catch(err => next(err));
+})
+
+
+
+
+module.exports = router
