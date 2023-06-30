f82d0c01fe1b3095200ddd872b9ea3242e7196c3
Juan Antonio Caballero <j.antoniocaballero@hotmail.es>
2023-06-30 12:20:13 +0200

    bcrypt

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
+const bcrypt = require("bcryptjs");
 const User = require("../models/User.model");
+const saltRounds = 10;
 
 
 
@@ -7,9 +9,23 @@ router.post('/signup', (req, res, next) => {
 
     const { username, email, password } = req.body
 
+    if (password.length < 4) {
+        res.status(400).json({ errorMessages: ["La contraseña debe tener al menos 4 caracteres."] })
+        return
+    }
     User
-        .create({ username, email, password })
-        .then(() => res.sendStatus(201))
+        .findOne({ email })
+        .then((foundUser) => {
+            if (foundUser) {
+                res.status(400).json({ errorMessages: ["El usuario ya existe."] })
+                return
+            }
+            const salt = bcrypt.genSaltSync(saltRounds);
+            const hasedPassword = bcrypt.hashSync(password, salt);
+
+            return User.create({ username, email, password: hasedPassword })
+        })
+        .then((createdUser) => res.sendStatus(201))
         .catch(err => next(err));
 })
 
